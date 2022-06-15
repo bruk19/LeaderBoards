4c8b6a65291122ba3bc4d1a8514e776944a0fa35
bruk19 <brukteshome21@gmail.com>
2022-06-15 14:34:14 +0300

    Configure ESLint, Stylelint and Webhint

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,23 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    add: './src/add.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
   module: {
     rules: [
-      {
+       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
